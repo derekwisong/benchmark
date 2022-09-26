565664b730f567eb5f33713af35d6071c0b598e3
Derek Wisong <derekwisong@gmail.com>
2022-09-26 18:14:13 -0400

    Add benchmarks square, randoms, open close, stat

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,56 @@
 #include <chrono>
-#include <cstddef>
 #include <cstdlib>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <utility>
 
-class TwoDoubles {
-    double x = 0;
-    double y = 0;
+#include <sys/stat.h>
+
+
+struct TwoDoubles {
+  double x = 0;
+  double y = 0;
 };
 
-template <typename Type>
-void malloc_free() {
-  auto ptr = std::malloc(sizeof(Type));
+template <size_t size> void malloc_free() {
+  auto ptr = std::malloc(size);
   std::free(ptr);
 }
 
 template <typename Type> void new_delete() {
   auto obj = new Type();
   delete obj;
+}
+
+template <int val> void square() {
+  int result = val * val;
+  (void) result;
+}
+
+void random_int() {
+  auto val = std::rand();
+  (void) val;
+}
+
+void random_double() {
+  auto val = static_cast<double>(std::rand()) / RAND_MAX;
+  (void) val;
+}
+
+void open_close_fstream(const char* path) {
+  std::fstream file;
+  file.open(path, std::ios::in);
+  file.close();
+}
+
+void open_close_fopen(const char* path) {
+  FILE* file = fopen(path, "rb");
+  fclose(file);
+}
+
+void stat_file(const char* path) {
+  struct stat st;
+  stat(path, &st);
 }
 
 template <typename Func> std::pair<size_t, double> repeat_for_time(Func &&func, unsigned int milliseconds) {
@@ -36,15 +69,21 @@ template <typename Func> std::pair<size_t, double> repeat_for_time(Func &&func, 
   return std::make_pair(counter, calls_per_second);
 }
 
-template <typename Func>
-void benchmark(Func &&func, unsigned int milliseconds, std::string description) {
+template <typename Func> void benchmark(std::string description, unsigned int milliseconds, Func &&func) {
   auto [count, calls_per_sec] = repeat_for_time(func, milliseconds);
-  std::cout << description << ": " << count << " calls in " << milliseconds << "ms (" << calls_per_sec << " calls/sec"
-            << std::endl;
+  std::cout << std::left << std::setw(20) << description << std::right << std::setw(12) << count << " calls in "
+            << milliseconds << "ms " << std::setw(20) << calls_per_sec << " calls/sec" << std::endl;
 }
 
-int main(int, char **) {
-    static const unsigned int RUNTIME = 500;
-    benchmark(malloc_free<TwoDoubles>, RUNTIME, "malloc/free (size of TwoDoubles)");
-    benchmark(new_delete<TwoDoubles>, RUNTIME, "new/delete of TwoDoubles");
+int main(int, char ** argv) {
+  static const unsigned int RUNTIME = 1000;
+  benchmark("new/delete", RUNTIME, new_delete<TwoDoubles>);
+  benchmark("malloc/free", RUNTIME, malloc_free<sizeof(TwoDoubles)>);
+  benchmark("square", RUNTIME, square<20>);
+  benchmark("random int", RUNTIME, random_int);
+  benchmark("random double", RUNTIME, random_double);
+  benchmark("open/close (fstream)", RUNTIME, [f = argv[0]]{open_close_fstream(f);});
+  benchmark("open/close (fopen)", RUNTIME, [f = argv[0]]{open_close_fopen(f);});
+  benchmark("stat", RUNTIME, [f = argv[0]]{stat_file(f);});
+
 }
